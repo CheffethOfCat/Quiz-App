ebe2efac6dfee4fdcb864a84c4a86ee1be815709
CheffethOfCat <132242256+CheffethOfCat@users.noreply.github.com>
2024-05-28 09:24:16 +1200

    quiz.py buttons
    Functional button actually added this time, waiting for repurpose, redesign, refunction and duplication.

--- quiz.py
+++ quiz.py
@@ -2,9 +2,14 @@ from tkinter import*
 from tkinter import messagebox
 import tkinter as tk
 
-
-
 window = tk.Tk()
-window.title("Quiz")
 window.geometry("1200x700")
+
+def helloCallBack ():
+    msg=messagebox.showinfo ("Hello Python", "Hello World.")
+B = Button(window, text = "Hello", command = helloCallBack)
+B.place(x=50,y=50)
+
 window.mainloop()
+
+#Comment
