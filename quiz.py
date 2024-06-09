f5ebbe7b5efae585bb9a77cb8d68997b2ddf71a5
CheffethOfCat <132242256+CheffethOfCat@users.noreply.github.com>
2024-06-10 10:49:17 +1200

    Title
    Added title, adding design to title soon.

--- quiz.py
+++ quiz.py
@@ -4,11 +4,16 @@ import tkinter as tk
 
 window = tk.Tk()
 window.geometry("1200x700")
+#Choosing quiz window size
+
+title_label = tk.Label(window, text="Welcome to My App", font=("Helvetica", 16))
+title_label.pack(pady=10)
 
 def helloCallBack ():
     msg=messagebox.showinfo ("Hello Python", "Hello World.")
 B = Button(window, text = "Hello", command = helloCallBack)
-B.place(x=50,y=50)
+B.place(x=600,y=350)
+#Button testing
 
 window.mainloop()
 
